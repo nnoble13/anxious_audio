845d5782e377866cc474e1d4bddf7dac70b44937
asia noble <nnoble.data@gmail.com>
2024-03-09 10:29:54 +0100

    directory skeleton

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = (Flask(__name__))
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'hello bros'
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True) #refresh with latest updates, threaded wont get overloaded processing multiple fiels at the same time
+ 
