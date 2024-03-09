8e1769afab27176481e3775b1cb6d40610e9936b
asia noble <nnoble.data@gmail.com>
2024-03-09 10:52:42 +0100

    first part of exercise done

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = (Flask(__name__))
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST']) #load page and render as home page, if request is post then do smth with form data
 def index():
-    return 'hello bros'
+    if request.method == 'POST':
+        print('Audio received')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True) #refresh with latest updates, threaded wont get overloaded processing multiple fiels at the same time
