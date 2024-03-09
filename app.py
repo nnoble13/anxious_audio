84c335d0c4826d10ed017c2183110e654c40c14f
asia noble <nnoble.data@gmail.com>
2024-03-09 11:03:15 +0100

    audio works

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import speech_recognition as sr #use googlecloud to convert audio to text
+
 
 app = (Flask(__name__))
 
@@ -6,6 +8,23 @@ app = (Flask(__name__))
 def index():
     if request.method == 'POST':
         print('Audio received')
+
+        if 'file' not in request.files: #redirects if file does not exist
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return redirect #returns user back to homepage if file is blank
+
+        if file:
+            recognizer = sr.Recognizer() #initialize instance if file exists
+            audioFile = sr.AudioFile(file) #create object of audio file
+            with audioFile as source:
+                data = recognizer.record(source)
+            text = recognizer.recognize_google(data, key=None) #you can use speech to text key if not None
+            print(text)
+
     return render_template('index.html')
 
 if __name__ == '__main__':
