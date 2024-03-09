125fd41c4b79277c484a9d4791d0ddb9d7da3c53
asia noble <nnoble.data@gmail.com>
2024-03-09 13:37:56 +0100

    added berta model for text

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect
 import speech_recognition as sr #use googlecloud to convert audio to text
-
+from transformers import pipeline
 
 app = (Flask(__name__))
+
+sentiment_pipeline = pipeline("sentiment-analysis", model="finiteautomata/bertweet-base-sentiment-analysis")
 
 @app.route('/', methods=['GET', 'POST']) #load page and render as home page, if request is post then do smth with form data
 def index():
     transcript=""
+    sentiment = ""
     if request.method == 'POST':
         print('Audio received')
 
@@ -26,7 +29,11 @@ def index():
             transcript = recognizer.recognize_google(data, key=None) #you can use speech to text key if not None
             print(transcript)
 
-    return render_template('index.html', transcript=transcript) #render the template with the transcript text from audio file
+        results = sentiment_pipeline(transcript)
+        sentiment = results[0]['label']  # Assuming you want the first result's label
+
+
+    return render_template('index.html', transcript=transcript, sentiment=sentiment) #render the template with the transcript text from audio file
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True) #refresh with latest updates, threaded wont get overloaded processing multiple fiels at the same time
--- readme.md
+++ readme.md
@@ -4,6 +4,8 @@
 
 This project is a speech recognition system designed to transcribe speech from `.wav` files. Currently, it features a Flask application to run the transcription service locally. The next phase will introduce sentiment analysis on the transcribed text to evaluate emotional tones.
 
+Right now it's using sentiment analysis on text that is transcribed, but further I'd like to use text and raw audio. Using Twitter-roberta-base-sentiment which is a roBERTa model trained on ~58M tweets and fine-tuned for sentiment analysis.
+
 ![This is the current progress](images/Capture.JPG "Work in Progress")
 
 ## Quick Start
